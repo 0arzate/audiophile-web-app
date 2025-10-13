43a5b556546dddcd9b2782f2800b95a1d25af3bc
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2025-10-13 07:09:22 -0600

    feat(config): 🔥 remove useles core page functions

--- app/utils/core-page.js
+++ app/utils/core-page.js
@@ -1,11 +1,5 @@
-import { html, LitElement } from 'lit'
+import { LitElement } from 'lit'
 import { LocalizeMixin } from '@open-cells/localize'
 import { PageMixin } from '@open-cells/page-mixin'
 
-export class CorePage extends PageMixin(LocalizeMixin(LitElement)) {
-  get renderHeader () {
-    return html`
-      <app-header></app-header>
-    `
-  }
-}
+export class CorePage extends PageMixin(LocalizeMixin(LitElement)) {}
